61371a4b98c25b486165b0e9fa0c1990e7241582
Sergio Holguin <sholguinc@uni.pe>
2022-09-14 16:11:07 -0500

    debugging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Uvicorn
+import uvicorn
 
 # FastAPI
 from fastapi import FastAPI
@@ -10,3 +12,5 @@ app = FastAPI()
 app.include_router(router)
 
 
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
