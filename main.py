fd2eaea9370bf664789ea11cdb5ae7e266a57313
Sergio Holguin <sholguinc@uni.pe>
2022-09-19 19:16:19 -0500

    CROS

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import uvicorn
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Router
 from controllers import router
@@ -14,11 +15,21 @@ from controllers import router
 # Middleware
 from middleware import process_time_header
 
+# CROS (Cros-Origin Resource Sharing)
+from origins import cros_origins
+
 
 # App
 app = FastAPI()
 app.include_router(router)
 app.add_middleware(BaseHTTPMiddleware, dispatch=process_time_header)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cros_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 if __name__ == '__main__':
